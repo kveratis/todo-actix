0a293dc2bff0edc09f057902f70959d7a0e07552
Daniel Petersen <daniel.petersen@rapidcyberdefense.com>
2020-06-05 18:15:29 -0600

    Simple JSON Server with configuration

---
+++ README.md
@@ -0,0 +1,15 @@
+# Actix TODO
+
+Actix based todo app in rust.
+
+## To Run
+
+cargo run
+
+## Check for errors
+
+cargo check
+
+## How Created
+
+cargo init --bin todo-actix
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+mod config;
+mod models;
+
+use crate::models::Status;
+use actix_web::{HttpServer, App, web, Responder};
+use std::io;
+use dotenv::dotenv;
+
+async fn status() -> impl Responder {
+    web::HttpResponse::Ok()
+        .json(Status { status: "Ok".to_string() })
+}
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+
+    dotenv().ok();
+
+    let config = crate::config::Config::from_env().unwrap();
+
+    println!("Starting server at http://{}:{}/", config.server.host, config.server.port);
+
+    HttpServer::new(|| (
+        App::new()
+            .route("/", web::get().to(status))
+    ))
+    .bind(format!("{}:{}", config.server.host, config.server.port))?
+    .run()
+    .await
+}
